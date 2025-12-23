21568f595600e80fd5f38e87a7c420f6a1de905c
Top-leadership77 <hichamdakhlaoui1@gmail.com>
2025-12-23 12:39:52 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-// ===== Firebase Config =====
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAJxzFknapSh0fnPfIXwEEK3OijSKlqxco",
-  authDomain: "kiosque-wazzani.firebaseapp.com",
-  projectId: "kiosque-wazzani",
-  storageBucket: "kiosque-wazzani.appspot.com",
+  authDomain: "admin-ash-f622c.firebaseapp.com",
+  projectId: "admin-ash-f622c",
+  storageBucket: "admin-ash-f622c.appspot.com",
   messagingSenderId: "76505742154",
   appId: "1:76505742154:web:dc4f190f9f2de80acb1fb3"
 };
-
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
